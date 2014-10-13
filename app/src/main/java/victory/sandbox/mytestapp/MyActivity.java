eb6f902830b5c9bd5c2280833db2d4cc4914e10f
Victory <victory@dfhu.org>
2014-10-13 18:59:36 -0400

    init commit

---
+++ app/src/main/java/victory/sandbox/mytestapp/MyActivity.java
@@ -0,0 +1,61 @@
+package victory.sandbox.mytestapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MyActivity extends Activity {
+    private Button toastThis;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        final TextView helloWorldText = (TextView)findViewById(R.id.helloWorld);
+        Button clickToDo = (Button)findViewById(R.id.clickToDo);
+        clickToDo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (helloWorldText.getText().equals(getString(R.string.hello_world))) {
+                    helloWorldText.setText(getString(R.string.iLikeToClick));
+                } else {
+                    helloWorldText.setText(getString(R.string.hello_world));
+                }
+            }
+        });
+
+        toastThis = (Button)findViewById(R.id.toastThis);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings || super.onOptionsItemSelected(item)) {
+            return true;
+        }
+        else return false;
+    }
+
+    public void doToast(View view) {
+        String theResponse = "This is a sweet response";
+        Toast.makeText(this, theResponse, Toast.LENGTH_SHORT).show();
+    }
+}
